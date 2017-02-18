6a47c28986e4e426b417921d10fe6799348ab5cd
Ryan Hughes <ryan@oodle.io>
2017-02-18 16:54:47 -0500

    Setup Gulp & added Concise.css

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var cp          = require('child_process');
+
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( 'bundle' , ['exec', 'jekyll', 'build', '-I', '--limit_posts', '10'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', ['compile-sass'], function() {
+    return gulp.src('assets/main.css')
+        .pipe(gulp.dest('_site/assets'))
+        .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('compile-sass', function (done) {
+    return cp.spawn( 'concisecss' , ['compile', '_scss/main.scss', 'assets/main.css'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
